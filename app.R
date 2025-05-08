ad2c055def9d0c94f727f6ab9bd42be38475d2bd
Sam El-Kamand <sam.elkamand@gmail.com>
2025-05-08 10:07:11 +1000

    fix: removed redundant app.R
    Was only important for webassembly compilation, but now we have the 'webassembly' branch to solve the same problem

--- app.R
+++
@@ -1,7 +0,0 @@
-message("Loading pacakge with pkgload::load_all ")
-pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
-message("Updating options")
-options("golem.app.prod" = TRUE)
-message("About to run app")
-run_app()
-
